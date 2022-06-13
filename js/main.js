f826810dc70c02366e63f9ef0f6db02c2eeedef3
bijayshrestha14 <bijay@genesiswtech.com>
2022-06-13 19:19:22 +0545

    index.html main.js style.css udpated

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    nav:true,
-    dots:false,
-    autoplay:true,
-    autoplayhoverpause:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-})
+$(".banner-section .owl-carousel").owlCarousel({
+  loop: true,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayhoverpause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+$(".activites-slider .owl-carousel").owlCarousel({
+  loop: true,
+  nav: true,
+  margin: 50,
+  dots: false,
+  autoplay: true,
+  autoplayhoverpause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+// Responsive Header
+$(".navigation  ul > li.has-submenu").append('<i class="fa-solid fa-angle-down"></i>');
+
+$(".burger-menu").on("click", function () {
+  $("body").addClass("show-menu");
+});
+
+$(".overlay-header").on("click", function () {
+  $("body").removeClass("show-menu");
+});
